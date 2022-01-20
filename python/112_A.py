06bfce211cab3cc0c4428a539c7b3aa9a7eeed17
Afif Malghani <50712084+afif-malghani@users.noreply.github.com>
2022-01-21 04:24:52 +0500

    added accepted solution for 112_A in python

---
+++ python/112_A.py
@@ -0,0 +1,16 @@
+def sol():
+    str1 = input().lower()
+    str2 = input().lower()
+    if str1 < str2:
+        return -1
+    elif str2 <str1:
+        return 1
+    elif str1 == str2:
+        return 0
+
+print(sol())
+
+
+# aslkjlkasdd
+# 1+19+12+
+# asdlkjdajwi
